026098f2f0b51d86d54f198da64a66f7aa986ca2
Tracy Wallace <tswwci@hotmail.com>
2022-11-15 16:49:06 -0500

    Moved Python to folder, added c#

---
+++ csharp-webserver/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace cloud_utilities.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
