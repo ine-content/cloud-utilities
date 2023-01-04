5d4cd545717410e6bbbefe0145b0f48f8a727619
Tracy Wallace <tswwci@hotmail.com>
2023-01-04 14:21:10 -0500

    Added demos folder & content.

---
+++ demos/vmss/updateCLI.ps1
@@ -0,0 +1,7 @@
+az vmss extension set `
+  --publisher Microsoft.Azure.Extensions `
+  --version 2.0 `
+  --name CustomScript `
+  --resource-group 02tasks `
+  --vmss-name vmss `
+  --settings @updateapp.json
