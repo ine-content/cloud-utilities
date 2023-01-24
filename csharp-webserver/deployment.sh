1636e37eaaf9e8eeadc2c9f11e81783cd09741c9
Tracy Wallace <tswwci@hotmail.com>
2023-01-24 13:36:21 -0500

    Removed docker compose from deployment script.

--- csharp-webserver/deployment.sh
+++ csharp-webserver/deployment.sh
@@ -11,5 +11,5 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io
 docker run --name webapp -p 80:80 -d --restart always --env-file /var/lib/mywebapp/docenv twallace27603/inedemowebserver:latest
