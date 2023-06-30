030c6d604032b73ebb220598055543bfd45b9cca
Tracy Wallace <tswwci@hotmail.com>
2023-06-30 10:41:57 -0400

    Updated the password to match AWS

--- mysql/mysql_install.sh
+++ mysql/mysql_install.sh
@@ -1,8 +1,8 @@
 apt update
 apt install -y mariadb-server
 systemctl start mariadb.service
-mysql -e "GRANT ALL PRIVILEGES ON *.* to 'student@%' IDENTIFIED BY '\!NEPassw0[]rd\!';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* to 'student' IDENTIFIED BY '\!NEPassw0[]rd\!';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* to 'student@%' IDENTIFIED BY '\!NEPassw0rd\![]123';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* to 'student' IDENTIFIED BY '\!NEPassw0rd\![]123';"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 echo '[mysqld]' > /etc/my.cnf
